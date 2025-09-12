eec54d7a228b87bc0c7b23446bd7d0097c52b95b
Compkev22 <kvelasquez-2022473@kinal.edu.gt>
2025-09-11 21:43:28 -0600

    T8: Se creo la Parte #3 del Mantenimiento de la Entidad Usuario

---
+++ src/main/java/org/code_wizards/Sistema_Budget/persistence/UsuarioEntityRepository.java
@@ -0,0 +1,70 @@
+package org.code_wizards.Sistema_Budget.persistence;
+
+
+import org.code_wizards.Sistema_Budget.dominio.dto.ModUsuarioDto;
+import org.code_wizards.Sistema_Budget.dominio.dto.UsuarioDto;
+import org.code_wizards.Sistema_Budget.dominio.repository.UsuarioRepository;
+import org.code_wizards.Sistema_Budget.persistence.crud.CrudUsuarioEntity;
+import org.code_wizards.Sistema_Budget.persistence.entity.UsuarioEntity;
+import org.code_wizards.Sistema_Budget.persistence.mapper.UsuarioMapper;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UsuarioEntityRepository implements UsuarioRepository {
+    private final CrudUsuarioEntity crudUsuarioEntity;
+    private final UsuarioMapper usuarioMapper;
+
+
+    public UsuarioEntityRepository(CrudUsuarioEntity crudUsuarioEntity, UsuarioMapper usuarioMapper) {
+        this.crudUsuarioEntity = crudUsuarioEntity;
+        this.usuarioMapper = usuarioMapper;
+    }
+
+    @Override
+    public List<UsuarioDto> obtenerTodo() {
+        return this.usuarioMapper.toDto(this.crudUsuarioEntity.findAll());
+    }
+
+    @Override
+    public UsuarioDto buscarPorCodigo(Long codigo) {
+        return this.usuarioMapper.toDto(this.crudUsuarioEntity.findById(codigo).orElse(null));
+    }
+
+    @Override
+    public UsuarioDto guardarUsuario(UsuarioDto usuarioDto) {
+        if (this.crudUsuarioEntity.findFirstByNombre(usuarioDto.nameUser()) != null) {
+            throw new UsuarioYaExisteException(usuarioDto.nameUser());
+        }
+
+        UsuarioEntity usuario = new UsuarioEntity();
+        // usuario = this.usuarioMapper.toEntity(usuarioDto)
+        this.crudUsuarioEntity.save(usuario);
+        return this.usuarioMapper.toDto(usuario);
+    }
+
+    @Override
+    public UsuarioDto modficarUsuario(Long codigo, ModUsuarioDto modUsuario) {
+        UsuarioEntity usuario = this.crudUsuarioEntity.findById(codigo).orElse(null);
+        //Excepcion
+        if (usuario == null) {
+            throw new UsuarioNoExisteException(codigo);
+        }
+        this.usuarioMapper.modificarEntityFromDto(modUsuario, usuario);
+        return usuarioMapper.toDto(this.crudUsuarioEntity.save(usuario));
+    }
+
+    @Override
+    public void eliminarUsuario(Long codigo) {
+        UsuarioEntity usuario = this.crudUsuarioEntity.findById(codigo).orElse(null);
+        this.crudUsuarioEntity.delete(usuario);
+        //Excepcion
+        if (usuario == null) {
+            throw new UsuarioNoExisteException(codigo);
+        } else {
+            this.crudUsuarioEntity.deleteById(codigo);
+        }
+    }
+}
