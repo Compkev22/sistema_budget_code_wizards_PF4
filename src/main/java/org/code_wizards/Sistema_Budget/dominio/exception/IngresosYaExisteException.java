0f14d530525eb49f0d274366fc625e300f09c5bb
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2025-09-15 12:46:45 -0600

    T38: Crear Parte #3 del Mantenimiento de la Entidad Ingresos

--- src/main/java/org/code_wizards/Sistema_Budget/dominio/exception/IngresosYaExisteException.java
+++ src/main/java/org/code_wizards/Sistema_Budget/dominio/exception/IngresosYaExisteException.java
@@ -1,4 +1,8 @@
 package org.code_wizards.Sistema_Budget.dominio.exception;
 
-public class IngresosYaExisteException {
+public class IngresosYaExisteException extends RuntimeException {
+
+    public IngresosYaExisteException(Integer idBudget, String descriptionEntry) {
+        super("El ingreso con Presupuesto ID: " + idBudget + " y Descripción: '" + descriptionEntry + "' ya existe.");
+    }
 }
