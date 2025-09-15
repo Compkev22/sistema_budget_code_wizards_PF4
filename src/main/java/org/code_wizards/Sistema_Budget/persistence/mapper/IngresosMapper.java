bcbfc7ad2a739b704dbfa09b08f7bb9eebf0f658
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2025-09-15 12:50:48 -0600

    T41: Crear Parte #6 del Mantenimiento de la Entidad Ingresos

--- src/main/java/org/code_wizards/Sistema_Budget/persistence/mapper/IngresosMapper.java
+++ src/main/java/org/code_wizards/Sistema_Budget/persistence/mapper/IngresosMapper.java
@@ -1,4 +1,33 @@
 package org.code_wizards.Sistema_Budget.persistence.mapper;
 
+import org.code_wizards.Sistema_Budget.dominio.dto.IngresosDto;
+import org.code_wizards.Sistema_Budget.dominio.dto.ModIngresosDto;
+import org.code_wizards.Sistema_Budget.persistence.entity.IngresosEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface IngresosMapper {
+
+    @Mapping(source = "idPresupuesto", target = "idBudget")
+    @Mapping(source = "idCategoria", target = "idCategory")
+    @Mapping(source = "descripcionIngreso", target = "descriptionEntry")
+    @Mapping(source = "montoIngreso", target = "incomeAmount")
+    @Mapping(source = "fechaIngreso", target = "entryDate")
+    IngresosDto toDto(IngresosEntity entity);
+
+    List<IngresosDto> toDto(Iterable<IngresosEntity> entities);
+
+    @InheritInverseConfiguration
+    IngresosEntity toEntity(IngresosDto ingresosDto);
+
+    @Mapping(source = "idBudget", target = "idPresupuesto")
+    @Mapping(source = "idCategory", target = "idCategoria")
+    @Mapping(source = "descriptionEntry", target = "descripcionIngreso")
+    @Mapping(source = "incomeAmount", target = "montoIngreso")
+    void modificarEntityFromDto(ModIngresosDto mod, @MappingTarget IngresosEntity entity);
 }
--- src/main/java/org/code_wizards/Sistema_Budget/web/controller/IngresosController.java
+++ src/main/java/org/code_wizards/Sistema_Budget/web/controller/IngresosController.java
@@ -1,4 +1,70 @@
 package org.code_wizards.Sistema_Budget.web.controller;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.code_wizards.Sistema_Budget.dominio.dto.IngresosDto;
+import org.code_wizards.Sistema_Budget.dominio.dto.ModIngresosDto;
+import org.code_wizards.Sistema_Budget.dominio.service.IngresosService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ingreso")
+@Tag(name = "Ingreso", description = "Operaciones Crud para Ingresos dentro del Sistema_Budget")
 public class IngresosController {
+    private final IngresosService ingresosService;
+
+    public IngresosController(IngresosService ingresosService) {
+        this.ingresosService = ingresosService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<IngresosDto>> obtenerIngresos() {
+        return ResponseEntity.ok(this.ingresosService.obtenerTodo());
+    }
+
+    @GetMapping("/{idIngreso}")
+    @Operation(
+            summary = "Obtener los Ingresos por su identificador",
+            description = "Retorna los Ingresos que coincida con el identificador enviado",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ingresos no encontrados"),
+                    @ApiResponse(responseCode = "404", description = "Ingresos no encontrados", content = @Content)
+            }
+    )
+
+    public ResponseEntity<IngresosDto> buscarPorCodigo
+            (@Parameter(description = "Identificador de los Ingresos a recuperar", example = "3") @PathVariable Long idIngreso) {
+        return ResponseEntity.ok(this.ingresosService.buscarPorCodigo(idIngreso));
+    }
+
+    //Agregar
+    @PostMapping
+    public ResponseEntity<IngresosDto> guardarIngresos
+    (@RequestBody @Valid IngresosDto ingresosDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).
+                body(this.ingresosService.guardarIngresos(ingresosDto));
+    }
+
+    //Modificar
+    @PutMapping("/{idIngreso}")
+    public ResponseEntity<IngresosDto> modificarIngresos
+    (@PathVariable Long idIngreso, @RequestBody ModIngresosDto modIngresos) {
+        return ResponseEntity.ok(this.ingresosService.modificarIngresos(idIngreso, modIngresos));
+    }
+
+    //Eliminar
+    @DeleteMapping("/{idIngreso}")
+    public ResponseEntity<Void> eliminarCredenciales(@PathVariable Long idIngreso) {
+        this.ingresosService.eliminarIngresos(idIngreso);
+        return ResponseEntity.ok().build();
+    }
 }
