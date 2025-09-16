8f97bedc5aa9c804866bd25ef01ab83af77f482a
Compkev22 <kvelasquez-2022473@kinal.edu.gt>
2025-09-15 20:52:06 -0600

    T32: Se Creo la Parte #3 del Mantenimiento de la Entidad Transaccion

--- src/main/java/org/code_wizards/Sistema_Budget/persistence/crud/CrudTransaccionEntity.java
+++ src/main/java/org/code_wizards/Sistema_Budget/persistence/crud/CrudTransaccionEntity.java
@@ -1,5 +1,6 @@
 package org.code_wizards.Sistema_Budget.persistence.crud;
 
+import org.code_wizards.Sistema_Budget.persistence.entity.TransaccionEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CrudTransaccionEntity extends CrudRepository<TransaccionEntity, Long> {
