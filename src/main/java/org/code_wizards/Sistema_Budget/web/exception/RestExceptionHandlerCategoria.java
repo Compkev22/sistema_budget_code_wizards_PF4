c4f7e194cf05c3e6946d4adb91ec396096c434e3
Mcho2021304 <mcho-2021304@kinal.edu.gt>
2025-09-16 19:35:14 -0600

    Crear Parte #5 del Mantenimiento de la Entidad Categorías

---
+++ src/main/java/org/code_wizards/Sistema_Budget/web/exception/RestExceptionHandlerCategoria.java
@@ -0,0 +1,42 @@
+package org.code_wizards.Sistema_Budget.web.exception;
+import org.code_wizards.Sistema_Budget.dominio.exception.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class RestExceptionHandlerCategoria {
+
+    @ExceptionHandler(CategoriaExisteException.class)
+    public ResponseEntity<ErrorCategoria> handleCategoriaExiste (CategoriaExisteException ex) {
+        ErrorCategoria error = new ErrorCategoria("categoria-ya-existe", ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(CategoriaNoExisteException.class)
+    public ResponseEntity<ErrorCategoria> handleCategoriaNoExiste(CategoriaNoExisteException ex) {
+        ErrorCategoria error = new ErrorCategoria("categoria-no-existe", ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorCategoria>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<ErrorCategoria> errores = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errores.add(new ErrorCategoria(fieldError.getField(), fieldError.getDefaultMessage()));
+        });
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorCategoria> handleUnknownException(Exception ex) {
+        ErrorCategoria error = new ErrorCategoria("-error desconocido-", ex.getMessage());
+        return ResponseEntity.internalServerError().body(error);
+    }
+
+}
+
